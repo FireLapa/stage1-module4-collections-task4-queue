08a0b37e23fb07e54c30fb992823719abad4b815
Lapa <lapabig@gmail.com>
2023-06-05 17:34:02 +0600

    Updated

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,38 @@
 package com.epam.collections.queue;
 
+import com.sun.jdi.Value;
+
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+         arrayDeque.offer(firstQueue.poll());
+        arrayDeque.offer(firstQueue.poll());
+        arrayDeque.offer(secondQueue.poll());
+        arrayDeque.offer(secondQueue.poll());
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+
+            firstQueue.offer(arrayDeque.pollLast());
+            arrayDeque.offer(firstQueue.poll());
+            arrayDeque.offer(firstQueue.poll());
+
+            secondQueue.offer(arrayDeque.pollLast());
+            arrayDeque.offer(secondQueue.poll());
+            arrayDeque.offer(secondQueue.poll());
+        }
+
+        ArrayDeque<Integer> resultDeque = new ArrayDeque<>();
+        resultDeque.addAll(arrayDeque);
+        resultDeque.addAll(firstQueue);
+        resultDeque.addAll(secondQueue);
+
+        return resultDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,33 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            int dishNumber = queue.poll();
+
+            if (count % everyDishNumberToEat == 0) {
+                result.add(dishNumber);
+            } else {
+                queue.add(dishNumber);
+            }
+            count++;
+        }
+
+        return result;
     }
 }
